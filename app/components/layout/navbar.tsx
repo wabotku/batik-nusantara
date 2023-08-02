7cba1f554b6babc1f3031359bb2d8944307f6691
walid <walidshr8@gmail.com>
2023-08-02 16:06:50 +0700

    update navbar

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,5 +1,5 @@
 // Navbar.tsx
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "/public/image/logo.png";
 import Image from "next/image";
@@ -11,13 +11,32 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const navbarRef = useRef<HTMLDivElement | null>(null); // Explicit type annotation
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target as Node)) {
+      setMobileMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      window.addEventListener('click', handleClickOutside);
+    } else {
+      window.removeEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isMobileMenuOpen]);
+
   return (
-    <nav className="hover:bg-gray-800 bg-gray-100 bg-opacity-50 fixed w-[80vw] rounded-lg items-center justify-center left-1/2 transform -translate-x-1/2 translate-y-1/4">
+    <nav className="navbar hover:bg-gray-800 bg-gray-100 bg-opacity-50 fixed w-[80vw] rounded-lg items-center justify-center left-1/2 transform -translate-x-1/2 translate-y-4 md:translate-y-1/4" ref={navbarRef}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -29,29 +48,29 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
             <div className="ml-10 flex items-baseline space-x-4">
               <Link
                 to="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 Home
               </Link>
               <Link
                 to="/product"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 Products
               </Link>
               <Link
                 to="/about"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 About
               </Link>
               <Link
                 to="/login"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 Login
               </Link>
-              <div>
+              {/* <div>
                 <button
                   className={`bg-gray-200 dark:bg-gray-700 rounded-full p-2 focus:outline-none ${
                     darkMode ? "translate-x-0 bg-indigo-500" : "translate-x-0"
@@ -67,13 +86,13 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
                     }`}
                   />
                 </button>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="md:hidden">
             <button
               type="button"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-1 rounded-md"
+              className="btnToggle text-white-300 hover:bg-gray-700 hover:text-white px-2 py-1 rounded-md"
               onClick={toggleMobileMenu}
             >
               <svg
