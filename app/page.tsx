9c59ab3c73a77e050f13658d411a5f3414414e4f
wabot <walidshr8@gmail.com>
2023-08-13 20:20:47 +0700

    update analitic

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Base from "./components/layout/base";
 import Index from "./components/pages";
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
+
 // import About from "./components/pages/about";
 // import Product from "./components/pages/product";
 // import Login from "./components/pages/login";
 
 export default function Home() {
-  return <Base></Base>;
+  return (
+    <>
+      <Base></Base>
+      <Analytics />
+    </>
+  );
 }
